3ae97a4d33fbf172d2676381872508eac941aba1
Stefany <stefany.vasc.sa@gmail.com>
2020-02-28 15:12:19 -0300

    :construction: completion of form logic and retouching in css

--- src/main.js
+++ src/main.js
@@ -1,23 +1,29 @@
-const forms = document.getElementById("forms");
+var resultado = document.getElementById("local");
 
-const horasDiarias = document.querySelector("#horasDiarias").value;
-const diasEfetivos = document.querySelector("#diasEfetivos").value;
-const diasFerias = document.querySelector("#diasFerias").value;
-const valorProjeto = document.querySelector("#valorProjeto").value;
+var calc = document.getElementById("buttonCalcular");
 
-const resultado = document.getElementById("local");
+calc.addEventListener("click", calcHoras);
 
-const handleClick = () => {
-  calculando();
-  forms.addEventListener("submit", e => {
-    e.preventDefault();
-  });
-};
-
-const calculando = () => {
-  const valorHora =
-    valorProjeto / (diasEfetivos * 4 * horasDiarias) +
-    diasFerias * diasEfetivos * horasDiarias;
-
-  resultado.textContent = `O valor da sua hora é: R$${valorHora.toFixed(2)}`;
-};
+function calcHoras() {
+  var horasDiarias = document.getElementById("horasDiarias").value;
+  var diasEfetivos = document.getElementById("diasEfetivos").value;
+  var diasFerias = document.getElementById("diasFerias").value;
+  var valorProjeto = document.getElementById("valorProjeto").value;
+  if (
+    horasDiarias == "" ||
+    diasEfetivos == "" ||
+    diasFerias == "" ||
+    valorProjeto == ""
+  ) {
+    alert(
+      "Por favor digite os valores númericos para podermos calcular quanto vale a sua hora!"
+    );
+  } else {
+    valorHora =
+      valorProjeto / (diasEfetivos * 4 * horasDiarias) +
+      diasFerias * diasEfetivos * horasDiarias;
+    resultado.innerHTML = `<p class= "resultadoColor">O valor da sua hora é: R$ ${valorHora.toFixed(
+      2
+    )}</p>`;
+  }
+}
